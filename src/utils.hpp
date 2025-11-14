a2cc0c8d21a9ea77c1ad06b1124dc5ec2afa8f40
Big-Iron-Cheems <52252627+Big-Iron-Cheems@users.noreply.github.com>
2025-11-14 17:10:58 +0100

    Use templated fns for port I/O
    - ASM calls appropriate `inx` instructions and uses `Nd` for immediate ports
    - added `const`s where possible
    - fixed `ceil_div` calculation
    
    Older I/O fns are kept as thin wrappers

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <cstdint>
 
 namespace cosmos::utils {
@@ -7,11 +8,11 @@ namespace cosmos::utils {
     void halt();
 
     void memset(void* dst, uint8_t value, uint64_t size);
-    void memcpy(void* dst, void* src, uint64_t size);
+    void memcpy(void* dst, const void* src, uint64_t size);
 
-    template <typename T>
+    template <std::integral T>
     T ceil_div(T a, T b) {
-        return (a + b) / b;
+        return (a + b - 1) / b;
     }
 
     template <typename T>
@@ -19,45 +20,69 @@ namespace cosmos::utils {
         return (value + (alignment - 1)) & ~(alignment - 1);
     }
 
-    // Byte
+    /// Available port widths for port I/O operations (8, 16, or 32 bits)
+    template <typename T>
+    concept port_width = std::same_as<T, uint8_t> || std::same_as<T, uint16_t> || std::same_as<T, uint32_t>;
 
-    inline uint8_t byte_in(uint16_t port) {
-        uint8_t result;
-        __asm__ volatile("in %%dx, %%al" : "=a"(result) : "d"(port));
-        return result;
+    /// Port read
+    template <port_width T>
+    T port_in(uint16_t port) {
+        T value{};
+        if constexpr (std::same_as<T, uint8_t>) { // NOLINT(*-branch-clone)
+            __asm__ volatile("inb %1, %0" : "=a"(value) : "Nd"(port));
+        } else if constexpr (std::same_as<T, uint16_t>) {
+            __asm__ volatile("inw %1, %0" : "=a"(value) : "Nd"(port));
+        } else { // uint32_t
+            __asm__ volatile("inl %1, %0" : "=a"(value) : "Nd"(port));
+        }
+        return value;
     }
 
-    inline void byte_out(uint16_t port, uint8_t data) {
-        __asm__ volatile("out %%al, %%dx" ::"a"(data), "d"(port));
+    /// Port write
+    template <port_width T>
+    void port_out(uint16_t port, T value) {
+        if constexpr (std::same_as<T, uint8_t>) { // NOLINT(*-branch-clone)
+            __asm__ volatile("outb %0, %1" ::"a"(value), "Nd"(port));
+        } else if constexpr (std::same_as<T, uint16_t>) {
+            __asm__ volatile("outw %0, %1" ::"a"(value), "Nd"(port));
+        } else { // uint32_t
+            __asm__ volatile("outl %0, %1" ::"a"(value), "Nd"(port));
+        }
+    }
+
+    // Byte
+
+    inline uint8_t byte_in(const uint16_t port) {
+        return port_in<uint8_t>(port);
+    }
+
+    inline void byte_out(const uint16_t port, const uint8_t data) {
+        port_out<uint8_t>(port, data);
     }
 
     // Short
 
-    inline uint16_t short_in(uint16_t port) {
-        uint16_t result;
-        __asm__ volatile("in %%dx, %%ax" : "=a"(result) : "d"(port));
-        return result;
+    inline uint16_t short_in(const uint16_t port) {
+        return port_in<uint16_t>(port);
     }
 
-    inline void short_out(uint16_t port, uint16_t data) {
-        __asm__ volatile("out %%ax, %%dx" ::"a"(data), "d"(port));
+    inline void short_out(const uint16_t port, const uint16_t data) {
+        port_out<uint16_t>(port, data);
     }
 
     // Int
 
-    inline uint32_t int_in(uint16_t port) {
-        uint32_t result;
-        __asm__ volatile("in %%dx, %%eax" : "=a"(result) : "d"(port));
-        return result;
+    inline uint32_t int_in(const uint16_t port) {
+        return port_in<uint32_t>(port);
     }
 
-    inline void int_out(uint16_t port, uint32_t data) {
-        __asm__ volatile("out %%eax, %%dx" ::"a"(data), "d"(port));
+    inline void int_out(const uint16_t port, const uint32_t data) {
+        port_out<uint32_t>(port, data);
     }
 
     // Other
 
     inline void wait() {
-        byte_out(0x80, 0);
+        port_out<uint8_t>(0x80, 0);
     }
 } // namespace cosmos::utils
